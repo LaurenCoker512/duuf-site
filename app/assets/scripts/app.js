ee0f551607eaa62861db566bd4bec08c552f549a
Lauren <laurenekcoker@gmail.com>
2018-03-05 13:30:27 -0600

    Added waypoints for scrolling

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,7 @@
 import MobileMenu from "./modules/mobile-menu";
+import RevealOnScroll from "./modules/reveal-on-scroll";
+import StickyHeader from "./modules/sticky-header";
 
 var mobileMenu = new MobileMenu();
+var revealOnScroll = new RevealOnScroll();
+var stickyHeader = new StickyHeader();
