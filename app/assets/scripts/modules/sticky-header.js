ce75b34aa8e5b7c82466edaeffc208df2fc8580e
Lauren <laurenekcoker@gmail.com>
2018-03-09 16:44:15 -0600

    Restyled home page

--- app/assets/scripts/modules/sticky-header.js
+++ app/assets/scripts/modules/sticky-header.js
@@ -5,6 +5,7 @@ import smoothScroll from "jquery-smooth-scroll";
 class StickyHeader {
     constructor() {
         this.headerLogo = $(".header__logo");
+        this.header = $(".header");
         this.headerTriggerElement = $(".large-hero__title-small");
         this.createHeaderWaypoint();
     }
@@ -15,8 +16,10 @@ class StickyHeader {
             element: this.headerTriggerElement[0],
             handler: function(direction) {
                 if (direction == "down") {
+                    that.header.addClass("header--reveal");
                     that.headerLogo.addClass("header--scroll");
                 } else {
+                    that.header.removeClass("header--reveal");
                     that.headerLogo.removeClass("header--scroll");
                 }
             }
